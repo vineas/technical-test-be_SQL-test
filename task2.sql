1d43cc1fbde0543d9f97261449d9f4abcdf3b739
vineas <alvienasyandika@gmail.com>
2023-10-01 19:52:56 +0700

    edit task 2

---
+++ task2.sql
@@ -0,0 +1,8 @@
+-- Semua transaksi yang dibuat oleh John Michael diurutkan berdasarkan nomor rekening dan tanggal transaksi.
+
+SELECT T.*, C.CUST_FIRSTNAME || ' ' || C.CUST_LASTNAME AS FULL_NAME
+FROM TRANSACTION T
+JOIN ACCOUNT A ON T.TRS_FROM_ACCOUNT = A.ACC_NUMBER
+JOIN CUSTOMER C ON A.ACC_OWNER = C.CUST_ID
+WHERE C.CUST_FIRSTNAME = 'John' AND C.CUST_LASTNAME = 'Michael'
+ORDER BY T.TRS_FROM_ACCOUNT, T.TRS_DATE;
